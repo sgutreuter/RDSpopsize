72793766663364c38ff971e5c4c62ba068a3d5ee
Steve Gutreuter <wqh4@cdc.gov>
2019-11-19 14:54:16 -0500

    Added RDSpopsize-package.R.

---
+++ R/RDSpopsize-package.R
@@ -0,0 +1,14 @@
+#' RDSpopsize
+#'
+#' Estimation of the size of key populations from respondent-driven
+#' sampling.  Currently provides only the capability to estimate population
+#' size from two-source capture-recapture sampling or, equivalently, the
+#' "multiplier method" commonly used in epidemiology.
+#'
+#' Currently the package consists of the single function \code{RDS_2S_PSE}.
+#'
+#' @name RDSpopsize
+#' @docType package
+#' @author Steve Gutreuter: \email{sgutreuter@@gmail.gov}
+#'
+NULL
